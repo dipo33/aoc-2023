a32dcc93622e0178073de928505424fd34eda337
Matej Dipčár <mato.dipcar@gmail.com>
2023-12-02 18:39:25 +0100

    feat: update template to not show unused variable warning

--- template/src/part2.rs
+++ template/src/part2.rs
@@ -1,6 +1,7 @@
 use std::fs;
 use std::path::Path;
 
+#[allow(unused_variables)]
 pub fn execute<P: AsRef<Path>>(path: P, name: &str, print: bool) {
     let contents: String = fs::read_to_string(path)
         .expect("Should have been able to read the file");
