10c2712ac2c83d3bc02ed33cb086ce12edbcecb3
Matej Dipčár <mato.dipcar@gmail.com>
2023-12-02 23:07:53 +0100

    feat: add default result print in template

--- template/src/part2.rs
+++ template/src/part2.rs
@@ -6,5 +6,9 @@ pub fn execute<P: AsRef<Path>>(path: P, name: &str, print: bool) -> %%RESULT_TYP
     let contents: String = fs::read_to_string(path)
         .expect("Should have been able to read the file");
 
+    if print {
+        println!("{} Result: {}", name, %%RESULT_TYPE%%::default());
+    }
+
     %%RESULT_TYPE%%::default()
 }
