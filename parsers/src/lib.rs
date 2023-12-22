577227b355e14a73eb52b991dae7187ab86a5de3
dipo33 <mato.dipcar@gmail.com>
2023-12-23 00:57:26 +0100

    feat: implement `fold_separated_list1` parser

--- parsers/src/lib.rs
+++ parsers/src/lib.rs
@@ -1,8 +1,9 @@
 use std::str::FromStr;
 
+use nom::{InputLength, IResult, Parser};
 use nom::character::complete::digit1;
 use nom::combinator::map;
-use nom::IResult;
+use nom::error::{ErrorKind, ParseError};
 
 /// Parses an integer value from the given input string.
 ///
@@ -20,4 +21,55 @@ pub fn integer<T>(input: &str) -> IResult<&str, T>
         <T as FromStr>::Err: std::fmt::Debug,
 {
     map(digit1, |s: &str| s.parse::<T>().unwrap())(input)
+}
+
+pub fn fold_separated_list1<I, O, O2, E, F, G, H, S, R>(
+    mut sep: S,
+    mut f: F,
+    mut init: H,
+    mut g: G,
+) -> impl FnMut(I) -> IResult<I, R, E>
+    where
+        I: Clone + InputLength,
+        F: Parser<I, O, E>,
+        S: Parser<I, O2, E>,
+        G: FnMut(R, O) -> R,
+        H: FnMut() -> R,
+        E: ParseError<I>,
+{
+    move |mut i: I| {
+        let mut acc = init();
+
+        // Parse the first element
+        match f.parse(i.clone()) {
+            Err(e) => return Err(e),
+            Ok((i1, o)) => {
+                acc = g(acc, o);
+                i = i1;
+            }
+        }
+
+        loop {
+            let len = i.input_len();
+            match sep.parse(i.clone()) {
+                Err(nom::Err::Error(_)) => return Ok((i, acc)),
+                Err(e) => return Err(e),
+                Ok((i1, _)) => {
+                    // infinite loop check: the parser must always consume
+                    if i1.input_len() == len {
+                        return Err(nom::Err::Error(E::from_error_kind(i1, ErrorKind::SeparatedList)));
+                    }
+
+                    match f.parse(i1.clone()) {
+                        Err(nom::Err::Error(_)) => return Ok((i, acc)),
+                        Err(e) => return Err(e),
+                        Ok((i2, o)) => {
+                            acc = g(acc, o);
+                            i = i2;
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
