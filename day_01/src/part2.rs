4faa459715361ba2002165540c0514d1f6d1aacc
dipo33 <mato.dipcar@gmail.com>
2023-12-22 14:06:48 +0100

    feat: day1 optimise

--- day_01/src/part2.rs
+++ day_01/src/part2.rs
@@ -5,17 +5,17 @@ use std::path::Path;
 use lazy_static::lazy_static;
 
 lazy_static! {
-    static ref DIGIT_LOOKUP: HashMap<&'static str, char> = {
+    static ref DIGIT_LOOKUP: HashMap<&'static str, u8> = {
         let mut lookup = HashMap::new();
-        lookup.insert("one", '1');
-        lookup.insert("two", '2');
-        lookup.insert("three", '3');
-        lookup.insert("four", '4');
-        lookup.insert("five", '5');
-        lookup.insert("six", '6');
-        lookup.insert("seven", '7');
-        lookup.insert("eight", '8');
-        lookup.insert("nine", '9');
+        lookup.insert("one", 1);
+        lookup.insert("two", 2);
+        lookup.insert("three", 3);
+        lookup.insert("four", 4);
+        lookup.insert("five", 5);
+        lookup.insert("six", 6);
+        lookup.insert("seven", 7);
+        lookup.insert("eight", 8);
+        lookup.insert("nine", 9);
 
         lookup
     };
@@ -38,14 +38,16 @@ impl SubstringIter for &str {
     }
 }
 
-fn get_digit(str: &str) -> Option<char> {
+fn get_digit(str: &str) -> Option<u8> {
     for key in DIGIT_LOOKUP.keys() {
         if str.starts_with(key) {
             return DIGIT_LOOKUP.get(key).cloned();
         }
     }
 
-    str.chars().next().filter(char::is_ascii_digit)
+    str.chars().next()
+        .filter(char::is_ascii_digit)
+        .map(|c| c.to_digit(10).unwrap() as u8)
 }
 
 pub fn execute<P: AsRef<Path>>(path: P, name: &str, print: bool) -> u32 {
@@ -57,8 +59,7 @@ pub fn execute<P: AsRef<Path>>(path: P, name: &str, print: bool) -> u32 {
         .split_whitespace()
         .map(|str| (str.substrings().map(get_digit).find(Option::is_some), str.rsubstrings().map(get_digit).find(Option::is_some)))
         .map(|(first, last)| (first.unwrap().unwrap(), last.unwrap().unwrap()))
-        .map(|(first, last)| (first.to_digit(10).unwrap(), last.to_digit(10).unwrap()))
-        .map(|(first, last)| first * 10 + last)
+        .map(|(first, last)| (first * 10 + last) as u32)
         .sum();
 
     if print {
